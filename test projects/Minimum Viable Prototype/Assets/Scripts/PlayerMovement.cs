03764ef816a411396279eb56d094dd542416d9f6
Ani B <zbaker985@gmail.com>
2025-06-03 19:39:59 -0500

    progress on platforms for MVP

--- test projects/Minimum Viable Prototype/Assets/Scripts/PlayerMovement.cs
+++ test projects/Minimum Viable Prototype/Assets/Scripts/PlayerMovement.cs
@@ -8,46 +8,80 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed; //horizontal movement
     public float jumpForce; //vertical movement
     private Rigidbody2D rb;
-    private bool touchingGround;    //is touching ground? for jump
     public bool movementOn; //player can move?
+    private bool isTouchingGround;    //player is touching the ground?
+    private float maxCoyoteTime;    //jump allowed for extra time after leaving platform
+    private float coyoteTimer;   //timer used to determine whether jump is allowed at that moment
+    private float jumpBufferStartTime;    //jump allowed for extra time before landing on platform
+    private float jumpBufferTimer;   //timer used to determine whether jump is allowed at that moment
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        touchingGround = false;
+        isTouchingGround = false;
         Physics2D.gravity = new Vector2(0, -75f);
         movementOn = true;
+        maxCoyoteTime = 0.1f;
+        jumpBufferStartTime = 0.2f;
+        jumpBufferTimer = jumpBufferStartTime;
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizInput = Input.GetAxis("Horizontal");
-
-        //horizontal movement
-        if(movementOn)
+        if (movementOn)
+        {
+            //horizontal movement
             rb.velocity = new Vector2(horizInput * moveSpeed, rb.velocity.y);
 
-        //jump
-        if (movementOn && touchingGround && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            touchingGround = false;
+
+            //start coyote buffer countdown
+            if (isTouchingGround)
+                coyoteTimer = maxCoyoteTime;
+            //decrement coyote buffer
+            coyoteTimer -= Time.deltaTime;
+
+            //start jump buffer countdown
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+                jumpBufferTimer = jumpBufferStartTime;
+            //decrement jump buffer timer
+            else
+                jumpBufferTimer -= Time.deltaTime;
+
+            //release jump early
+            if (rb.velocity.y > 0f && (Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.W)))
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce * 0.35f);
+                
+                print("abort jump " + jumpBufferTimer);
+                jumpBufferTimer = 0f;
+            }
+
+            //execute jump if within buffer and coyote allowance
+            if (jumpBufferTimer > 0f && coyoteTimer > 0f)
+            {
+                print("initiate jump " + jumpBufferTimer);
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                isTouchingGround = false;
+                jumpBufferTimer = 0f;
+            }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //land on platform
-        if(collision.gameObject.tag == "Platform")
+        if (collision.gameObject.tag == "Platform")
         {
             rb.velocity = new Vector2(rb.velocity.x, 0);
-            touchingGround = true;
+            isTouchingGround = true;
         }
     }
+
     private void OnCollisionExit2D(Collision2D collision)
     {
-        touchingGround = false;
+        isTouchingGround = false;
     }
 }
