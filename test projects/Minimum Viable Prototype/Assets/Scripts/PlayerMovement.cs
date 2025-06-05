d9b13c73ac619ffcfcdc5b8c76e2d3f9d3b69cfa
Ani B <zbaker985@gmail.com>
2025-06-04 19:12:41 -0500

    MVP jump bug fixing

--- test projects/Minimum Viable Prototype/Assets/Scripts/PlayerMovement.cs
+++ test projects/Minimum Viable Prototype/Assets/Scripts/PlayerMovement.cs
@@ -12,19 +12,17 @@ public class PlayerMovement : MonoBehaviour
     private bool isTouchingGround;    //player is touching the ground?
     private float maxCoyoteTime;    //jump allowed for extra time after leaving platform
     private float coyoteTimer;   //timer used to determine whether jump is allowed at that moment
-    private float jumpBufferStartTime;    //jump allowed for extra time before landing on platform
-    private float jumpBufferTimer;   //timer used to determine whether jump is allowed at that moment
+    private bool inJumpBuffer;  //player is within jump buffer range?
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         isTouchingGround = false;
+        inJumpBuffer = true;
         Physics2D.gravity = new Vector2(0, -75f);
         movementOn = true;
         maxCoyoteTime = 0.1f;
-        jumpBufferStartTime = 0.2f;
-        jumpBufferTimer = jumpBufferStartTime;
     }
 
     // Update is called once per frame
@@ -36,36 +34,23 @@ public class PlayerMovement : MonoBehaviour
             //horizontal movement
             rb.velocity = new Vector2(horizInput * moveSpeed, rb.velocity.y);
 
-
             //start coyote buffer countdown
             if (isTouchingGround)
                 coyoteTimer = maxCoyoteTime;
             //decrement coyote buffer
             coyoteTimer -= Time.deltaTime;
 
-            //start jump buffer countdown
-            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
-                jumpBufferTimer = jumpBufferStartTime;
-            //decrement jump buffer timer
-            else
-                jumpBufferTimer -= Time.deltaTime;
+            //execute jump if within buffer and coyote allowance
+            if ((isTouchingGround | inJumpBuffer || coyoteTimer > 0f) && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)))
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                isTouchingGround = false;
+            }
 
             //release jump early
             if (rb.velocity.y > 0f && (Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.W)))
             {
                 rb.velocity = new Vector2(rb.velocity.x, jumpForce * 0.35f);
-                
-                print("abort jump " + jumpBufferTimer);
-                jumpBufferTimer = 0f;
-            }
-
-            //execute jump if within buffer and coyote allowance
-            if (jumpBufferTimer > 0f && coyoteTimer > 0f)
-            {
-                print("initiate jump " + jumpBufferTimer);
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-                isTouchingGround = false;
-                jumpBufferTimer = 0f;
             }
         }
     }
@@ -82,6 +67,25 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        isTouchingGround = false;
+        if(collision.gameObject.tag == "Platform")
+        {
+            isTouchingGround = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            inJumpBuffer = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Platform")
+        {
+            inJumpBuffer = false;
+        }
     }
 }
